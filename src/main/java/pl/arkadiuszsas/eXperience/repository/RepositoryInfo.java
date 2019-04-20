ca407ee526e7e6a3790e08ad164b3c24eb8dbdf0
sas-arkadiusz <kontakt.sas.arkadiusz@gmail.com>
2019-04-20 17:48:54 +0200

    Created RepositoryInfo class that contains values that describe the requested latest modified repository

---
+++ src/main/java/pl/arkadiuszsas/eXperience/repository/RepositoryInfo.java
@@ -0,0 +1,21 @@
+package pl.arkadiuszsas.eXperience.repository;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Class contains values that describe the requested latest modified repository.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RepositoryInfo {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
